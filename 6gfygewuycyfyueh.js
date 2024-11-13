865a70ab9ac8fbac78266a0fa4b38886888ba955
azkaa2011 <azka050511@gmail.com>
2024-11-13 22:47:54 +0700

    Add files via upload

---
+++ 6gfygewuycyfyueh.js
@@ -0,0 +1,2 @@
+    function showScore() {window.location.href = 'https://docs.google.com/spreadsheets/d/170j2-eOXfFhgziVH0bgyGbAckozdmXXYTIubIjwdNnY/edit?usp=drivesdk';}
+
---
+++ h67gh6lp662h.js
@@ -0,0 +1,2 @@
+if(window.top!==window.self){window.top.location=window.self.location;}
+
